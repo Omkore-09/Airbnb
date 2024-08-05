670f1ac361c1d919310811c965fe34ba70c123d4
omkar kore <142180062+Omkore-09@users.noreply.github.com>
2024-08-05 18:42:17 +0530

    Update schema.js

--- schema.js
+++ schema.js
@@ -8,7 +8,9 @@ module.exports.listingSchema = Joi.object({
     country: Joi.string().required(),
     price: Joi.number().required().min(0),
     image: Joi.string().allow("", null),
+    category: Joi.string().allow( 'rooms', 'iconic-cities', 'mountains', 'castles', 'pools', 'camping', 'farms', 'arctic', 'domes', 'boats').required(),
   }).required(),
+  
 });
 
 
